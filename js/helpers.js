0fa7ef8b6b6c466026dfa02e91bc6c584ec5c96f
Ferxu01 <ferortjim@gmail.com>
2023-05-10 21:08:12 +0200

    Función para guardar datos en partida y creado modal de turno inicial

--- js/helpers.js
+++ js/helpers.js
@@ -53,3 +53,34 @@ function elegirTurnoJugador() {
 
     return turno === 0 ? 'jugador1' : 'jugador2';
 }
+
+//CREAR MODAL Y ESTABLECER ESTILOS PRINCIPALES
+function creaPropiedadesModal() {
+    let dialogo = document.createElement('dialog');
+    dialogo.style.padding = '25px';
+    dialogo.style.fontFamily = 'Arial';
+    dialogo.style.position = 'fixed';
+    dialogo.style.top = '50%';
+    dialogo.style.left = '50%';
+    dialogo.style.transform = 'translate(-50%, -50%)';
+    return dialogo;
+}
+
+function mostrarModalTurno() {
+    const turno = sessionStorage.getItem('turno');
+    const modal = creaPropiedadesModal();
+    dialog.innerHTML = `
+        <h3>El turno es del jugador ${turno}</h3>
+    `;
+
+    document.body.appendChild(modal);
+    modal.showModal();
+}
+
+function guardarInfoPartida({ tablero, nombres, puntuacionesPartida, turnoActual, numerosDisponibles }) {
+    sessionStorage.setItem('tablero', tablero);
+    sessionStorage.setItem('nombres', JSON.stringify(nombres));
+    sessionStorage.setItem('puntuaciones', JSON.stringify(puntuacionesPartida));
+    sessionStorage.setItem('turno', turnoActual);
+    sessionStorage.setItem('numeros', JSON.stringify(numerosDisponibles));
+}
