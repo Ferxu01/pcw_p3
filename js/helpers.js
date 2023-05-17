53fce26b0a5c32780dad82f7370e54e6c977a033
Ferxu01 <ferortjim@gmail.com>
2023-05-17 15:06:24 +0200

    Pequeños cambios en helpers

--- js/helpers.js
+++ js/helpers.js
@@ -16,11 +16,11 @@ function enviaJugadores(evt) {
 }
 
 function guardaJugadores(jugadores) {
-    sessionStorage.setItem('jugadores', JSON.stringify(jugadores));
+    sessionStorage.setItem('nombres', JSON.stringify(jugadores));
 }
 
 function obtieneJugadores() {
-    return JSON.parse(sessionStorage.getItem('jugadores'));
+    return JSON.parse(sessionStorage.getItem('nombres'));
 }
 
 function compruebaVacio(player1, player2) {
@@ -48,10 +48,23 @@ function obtenerNumerosAleatorios() { //NUMEROS ALEATORIOS PARA JUGAR
     return numeros;
 }
 
-function elegirTurnoJugador() {
+function elegirPrimerTurno() {
     let turno = Math.round(Math.random());
 
     return turno === 0 ? 'jugador1' : 'jugador2';
+}
+
+function creaNumerosDisponibles(numeros) {
+    const divNumsDisponibles = document.getElementById('numsDisponibles');
+    let html = '';
+    numeros.forEach(num => {
+        html += `
+            <button type="button">${num}</button>
+        `;
+    });
+
+    divNumsDisponibles.innerHTML = html;
+
 }
 
 //CREAR MODAL Y ESTABLECER ESTILOS PRINCIPALES
@@ -69,12 +82,31 @@ function creaPropiedadesModal() {
 function mostrarModalTurno() {
     const turno = sessionStorage.getItem('turno');
     const modal = creaPropiedadesModal();
-    dialog.innerHTML = `
+    modal.innerHTML = `
         <h3>El turno es del jugador ${turno}</h3>
+        <button class="modal" onclick="cerrarModal()">Cerrar</button>
     `;
 
     document.body.appendChild(modal);
     modal.showModal();
+}
+
+function mostrarModalAyuda() {
+    const modal = creaPropiedadesModal();
+    modal.innerHTML = `
+        <h3 style="text-align: center">Ayuda</h3>
+        <br>
+        <p style="text-align: justify">El juego consiste en ir colocando en las casillas vacías del tablero los números que se proporcionan en grupos de tres. Juegan dos jugadores por turnos. Si al colocar un número en una celda vacía, sumándole el que tiene arriba/abajo/izquierda/derecha se obtiene un múltiplo de 5, se limpian las casillas correspondientes y el resultado de la suma son los puntos que acumula el jugador, manteniendo el turno. El juego finaliza cuando ya no quedan casillas vacías en el tablero, ganando el jugador con mayor puntuación.</p>
+        <button class="modal" onclick="cerrarModal()">Cerrar</button>
+    `;
+
+    document.body.appendChild(modal);
+    modal.showModal();
+}
+
+function cerrarModal() {
+    document.querySelector('dialog').close();
+    document.querySelector('dialog').remove();
 }
 
 function guardarInfoPartida({ tablero, nombres, puntuacionesPartida, turnoActual, numerosDisponibles }) {
@@ -83,4 +115,117 @@ function guardarInfoPartida({ tablero, nombres, puntuacionesPartida, turnoActual
     sessionStorage.setItem('puntuaciones', JSON.stringify(puntuacionesPartida));
     sessionStorage.setItem('turno', turnoActual);
     sessionStorage.setItem('numeros', JSON.stringify(numerosDisponibles));
+}
+
+function eliminarInfoPartida() {
+    sessionStorage.removeItem('tablero');
+    sessionStorage.removeItem('nombres');
+    sessionStorage.removeItem('puntuaciones');
+    sessionStorage.removeItem('turno');
+    sessionStorage.removeItem('numeros');
+}
+
+function terminarPartida() {
+    eliminarInfoPartida();
+    location.href = 'index.html';
+}
+
+/* =========================================================================== */
+/* HELPERS PARA PREPARAR EL TABLERO DE LA PARTIDA */
+/* =========================================================================== */
+const ANCHO = 480;
+const ALTO = 360;
+const NUM_CELDAS = 4;
+
+function getAnchoCelda() {
+    return ANCHO / NUM_CELDAS;
+}
+
+function getAltoCelda() {
+    return ALTO / NUM_CELDAS;
+}
+
+function prepararCanvas() {
+    let cv = document.querySelector('#tableroJuego');
+
+    cv.width = ANCHO;
+    cv.height = ALTO;
+
+    ponerEventos();
+
+    return cv;
+}
+
+function ponerEventos() {
+    let cv = document.querySelector('#tableroJuego');
+
+    // cv.addEventListener('mousemove', function(evt){ // Para cuando el ratón esté por encima...
+    cv.addEventListener('click', evt => {
+        let x = evt.offsetX,
+            y = evt.offsetY,
+            anchoCelda = getAnchoCelda(),
+            altoCelda = getAltoCelda(),
+            fila,col;
+
+        fila = Math.floor(y / altoCelda);
+        col = Math.floor(x / anchoCelda);
+        console.log(`(x,y): (${fila}, ${col})`);
+    });
+}
+
+function creaDivisionesCanvas() {
+    let cv = document.querySelector('#tableroJuego'),
+        ctx = cv.getContext('2d'),
+        anchoCelda = getAnchoCelda(),
+        altoCelda = getAltoCelda();
+    
+    ctx.beginPath();
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = 2;
+
+    for (let i = 1; i < NUM_CELDAS; i++) {
+        //Verticales
+        ctx.moveTo(i * anchoCelda, 0);
+        ctx.lineTo( i * anchoCelda, cv.height);
+
+        //Horizontales
+        ctx.moveTo(0, i * altoCelda);
+        ctx.lineTo(cv.width, i * altoCelda);
+    }
+
+    ctx.stroke();
+}
+
+/* =========================================================================== */
+/* HELPERS PARA PREPARAR LA PAGINA DE LA PARTIDA */
+/* =========================================================================== */
+function creaMarcador() {
+    const jugadores = obtieneJugadores(),
+        turno = elegirPrimerTurno();
+    
+    let marcador = `
+        <table>
+            <thead>
+                <tr>
+                    <td>Turno</td>
+                    <td>Jugador</td>
+                    <td>Puntos</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>${turno === 'jugador1' ? '*' : ''}</td>
+                    <td>${jugadores.jugador1}</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>${turno === 'jugador2' ? '*' : ''}</td>
+                    <td>${jugadores.jugador2}</td>
+                    <td>0</td>
+                </tr>
+            </tbody>
+        </table>
+    `;
+
+    document.querySelector('div.scoreContainer').innerHTML = marcador;
 }
--- js/juego.js
+++ js/juego.js
@@ -6,10 +6,37 @@ document.addEventListener('DOMContentLoaded', async () => {
         location.href = 'index.html';
     } else { //SE ESTA JUGANDO PARTIDA
         if (obtienePartida()) { 
-            
+
         } else { //EXISTE PARTIDA Y SE VA A INICIAR
-            let tablero = await getTablero();
+            creaMarcador();
+            prepararCanvas();
+            creaDivisionesCanvas();
+
+            let turnoActual = elegirPrimerTurno(),
+                numerosDisponibles = obtenerNumerosAleatorios(),
+                nombresJugadores = obtieneJugadores();
             
+            let tablero = await getTablero();
+            tablero = tablero.TABLERO;
+            console.table(tablero);
+
+            creaNumerosDisponibles(numerosDisponibles);
+
+            guardarInfoPartida({
+                tablero,
+                nombres: {
+                    jugador1: nombresJugadores.jugador1,
+                    jugador2: nombresJugadores.jugador2
+                },
+                turnoActual,
+                numerosDisponibles,
+                puntuacionesPartida: {
+                    jugador1: 0,
+                    jugador2: 0
+                }
+            });
+
+            mostrarModalTurno();
         }
     }
 
